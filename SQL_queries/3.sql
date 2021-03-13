47776b32622bcf2a25a7ac8cef471ae079c00ef7
zhanaboty <49232254+zhanaboty@users.noreply.github.com>
2021-03-13 21:29:53 +0600

    Create 3.sql

---
+++ SQL_queries/3.sql
@@ -0,0 +1,1 @@
+INSERT INTO profile(first_name, last_name, student_id, photo, course, specialty_id, phone_number) VALUES(:first_name, :last_name, :student_id, :photo, :course, :specialty_id, :phone_number);
