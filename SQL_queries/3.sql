ee8058e1e18aa48a3d8958c6cd4f9b6d6d4e812f
zhanaboty <49232254+zhanaboty@users.noreply.github.com>
2021-03-13 21:37:52 +0600

    Update 3.sql

--- SQL_queries/3.sql
+++ SQL_queries/3.sql
@@ -1,1 +1,2 @@
+-- How can users fill their profile information?
 INSERT INTO profile(first_name, last_name, student_id, photo, course, specialty_id, phone_number) VALUES(:first_name, :last_name, :student_id, :photo, :course, :specialty_id, :phone_number);
